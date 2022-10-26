3a1993344cfdc8a888ada2af0291a377f06f3a5c
Alycra <kobe@franssen.xyz>
2022-10-26 15:28:53 -0400

    adding .env and making bot usable

--- rybet.py
+++ rybet.py
@@ -1,33 +1,33 @@
-# This example requires the 'message_content' intent.
-import os
 import discord
 import random
-import logging
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-
-token = ''
-
+import os
+from dotenv import load_dotenv
 intents = discord.Intents.default()
+intents.message_content = True
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
 client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'Logged in as {client.user}')
+    print(f'We have logged in as {client.user}')
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="RyBet's poker table"))
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-
+        
     if message.content.startswith('$ping'):
-        await message.channel.send('pong')
-    if message.content.startswith('$coinflip'):
-        result = random.randint(1,2)
-        output = "error"
-        if (result == 1):
-            output = "heads"
-        if (result == 2):
-            output = "tails"
-        await message.channel.send(output)
+        await message.channel.send(":ping_pong: | " + str(round(client.latency,2)) + "ms")
 
-client.run(token, log_handler=handler)
+    if message.content.startswith('$coinflip'):
+        choice = random.getrandbits(1)
+        if choice:
+            output = "Heads"
+        else:
+            output = "Tails"
+        await message.reply(":coin: | " + output)
+
+client.run(TOKEN)
