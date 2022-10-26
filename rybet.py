8fda5dfa3fdd16c6fe899e85e26b93b65666914c
Alycra <kobe@franssen.xyz>
2022-10-25 22:18:57 -0400

    adding coinflip

--- rybet.py
+++ rybet.py
@@ -1,23 +1,33 @@
 # This example requires the 'message_content' intent.
 import os
 import discord
+import random
+import logging
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 
-token = os.getenv("DISCORD_TOKEN")
-my_guild = os.getenv("DISCORD_GUILD")
+token = ''
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'Logged in as {client.user}')
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('$ping'):
+        await message.channel.send('pong')
+    if message.content.startswith('$coinflip'):
+        result = random.randint(1,2)
+        output = "error"
+        if (result == 1):
+            output = "heads"
+        if (result == 2):
+            output = "tails"
+        await message.channel.send(output)
 
-client.run(token)
+client.run(token, log_handler=handler)
