0ee76676d1f6e0cfacaaa11083a1cf35a19945c5
alycra <kobe@franssen.xyz>
2022-10-30 13:25:17 -0400

    double quotation to single

--- rybet.py
+++ rybet.py
@@ -7,20 +7,20 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.message_content = True
 load_dotenv()
-TOKEN = os.getenv("DISCORD_TOKEN")
+TOKEN = os.getenv('DISCORD_TOKEN')
 
-ADMIN = "762737260617269268"
+ADMIN = '762737260617269268'
 
-client = commands.Bot(command_prefix="$", intents=intents)
+client = commands.Bot(command_prefix='$', intents=intents)
 
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="RyBet's poker table"))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='RyBet's poker table'))
 
 @client.event
 async def on_message(message):
-    username = str(message.author).split("#")[0]
+    username = str(message.author).split('#')[0]
     channel = str(message.channel.name)
     content = str(message.content).lower().split()
 
@@ -35,59 +35,61 @@ async def on_message(message):
                 if 1 <= user_choice <=6:
                     if bot_choice == user_choice:
                         result = float(content[2]) * 3
-                        await message.reply(":game_die: " + str(bot_choice) + " | :white_check_mark: Player: " + username + " wins | $" + result)
+                        await message.reply(':game_die: ' + str(bot_choice) + ' | :white_check_mark: Player: ' + username + ' wins | $' + result)
                     else:
                         result = float(content[2]) * -1
-                        await message.reply(":x: " + str(bot_choice) + " | :x: House wins | $" + result)
+                        await message.reply(':x: ' + str(bot_choice) + ' | :x: House wins | $' + result)
                 else:
-                    await message.reply("Incorrect number range, for a dice: 1-6")
+                    await message.reply('Incorrect number range, for a dice: 1-6')
             elif len(content) > 1:
-                await message.reply("Incorrect usage, correct template: $dicethrow 5 0.25")
+                await message.reply('Incorrect usage, correct template: $dicethrow 5 0.25')
             else:
                 choice = random.randint(1,6)
-                await message.reply(":game_die: | " + str(choice))
+                await message.reply(':game_die: | ' + str(choice))
         except:
-            await message.reply("Incorect usage of command, use $help for correct usage")
+            await message.reply('Incorect usage of command, use $help for correct usage')
 
     if message.content.startswith('$dice'):
         choice = random.random()
         if choice <= 0.5:
-            state = "Loss"
-            emoji = ":x:"
+            state = 'Loss'
+            emoji = ':x:'
         if choice > 0.5:
-            state = "win"
-            emoji = ":white_check_mark:"
-        output = str(round(choice * 100, 2)) + "%"
-        await message.reply(emoji + " | " + output)
+            state = 'win'
+            emoji = ':white_check_mark:'
+        output = str(round(choice * 100, 2)) + '%'
+        await message.reply(emoji + ' | ' + output)
     await client.process_commands(message)
 
 @client.command() #ping command
 async def ping(ctx):
-    await ctx.send(":ping_pong: | " + str(round(client.latency,2)) + "ms")
+    await ctx.send(':ping_pong: | ' + str(round(client.latency,2)) + 'ms')
 
 @client.command() #coinflip
-async def coinflip(ctx, choice : str = None, amount : str = None):
+async def coinflip(ctx, choice : str = None, amount : float = None):
     bot_choice = random.getrandbits(1) #generate random bot number of 1 or 0
-    user_choice = -1
     if bot_choice: #apply a result to the number
-        output = "Heads"
+        output = 'Heads'
     else:
-        output = "Tails"
-    if choice != None and amount != None:
-        try:
-            if choice is "heads" or choice is "head": #taking the user input and converting it to binary
-                user_choice = 1
-            elif choice is "tails" or choice is "tail":
-                user_choice = 0
-            if user_choice == bot_choice: #if the users choice is the same as the bot
-                result = amount * 2
-                await ctx.reply(":coin: "+ output +" | :white_check_mark: Player Wins | $" + result)
-            else: #if the users choice is not the same as the bot
-                result = amount * -1
-                await ctx.reply(":coin: "+ output +" | :x: House wins | $" + result)
-        except:
-            await message.reply("Incorect usage of command, use $help for correct usage")
+        output = 'Tails'
+    if choice != None:
+        if amount != None:
+            try:
+                if choice is 'heads' or choice is 'head': #taking the user input and converting it to binary
+                    user_choice = 1
+                elif choice is 'tails' or choice is 'tail':
+                    user_choice = 0
+                if user_choice == bot_choice: #if the users choice is the same as the bot
+                    result = amount * 2.0
+                    await ctx.reply(':coin: '+ output +' | :white_check_mark: Player Wins | $' + str(result))
+                else: #if the users choice is not the same as the bot
+                    result = amount * -1.0
+                    await ctx.reply(':coin: '+ output +' | :x: House wins | $' + str(result))
+            except:
+                await message.reply('Incorect usage of command, use $help for correct usage')
+        else:
+            await message.reply('Please specifiy a bet wager')
     else:
-        await ctx.reply(":coin: | " + output)
+        await ctx.reply(':coin: | ' + output)
 
 client.run(TOKEN)
